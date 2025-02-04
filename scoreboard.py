f1ebb93412beb212a2ddc1977706f598e7b0c5bb
Ema028 <156372524+Ema028@users.noreply.github.com>
2025-02-04 16:38:19 -0300

    Update scoreboard.py
    added way to keep track of best record score

--- scoreboard.py
+++ scoreboard.py
@@ -6,20 +6,26 @@ align = "center"
 class Score(Turtle):
     def __init__(self):
         super().__init__()
+        self.score = 0
+        with open("data.txt") as file:
+            self.high_score = int(file.read())
         self.penup()
         self.hideturtle()
-        self.score = 0
         self.goto(x = 0, y = 270)
         self.scoreboard()
 
     def scoreboard(self):
-        self.write(f"score = {self.score}", font = font, align = align)
+        self.clear()
+        self.write(f"Score = {self.score} Record = {self.high_score}", font = font, align = align)
 
     def point(self):
         self.score += 1
-        self.clear()
         self.scoreboard()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("Game Over", font = font, align = align)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode = "w") as file:
+                file.write(str(self.high_score))
+        self.score = 0
+        self.scoreboard()
